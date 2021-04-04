9ed031a4d40488cc50d75a7f0d89ede3f40db5eb
Lindsay201291 <lrodriguez.posma@gmail.com>
2021-04-04 16:45:54 -0400

    Creando estructura base del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { CharacterListComponent } from './character/components/character-list/character-list.component';
+import { ProfessorListComponent } from './professor/components/professor-list/professor-list.component';
+import { StudentListComponent } from './student/components/student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    CharacterListComponent,
+    ProfessorListComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule
